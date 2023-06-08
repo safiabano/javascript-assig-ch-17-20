74ddf60915ff37c9914a83bfe4d43588b4deebe6
safiabano <safiabano736@gmail.com>
2023-06-08 10:16:11 +0500

    assignment

---
+++ assets/js/app.js
@@ -0,0 +1,92 @@
+//===========chap 17-20 ARRAY AND LOOP =====================
+
+//========Q-1==========
+// let arr = [[]];
+
+//=========Q-2 =========
+// let matrix = [[0,1,2,3],[1,0,1,2],[2,1,0,1]];
+
+//=========Q-3 ==========
+//   for(let i = 1; i <=10 ;i++) {
+//   console.log(i)
+// }
+
+//========Q-4===========
+// let table = prompt("Enter a number to show its multiplication table")
+// let len = prompt("Enter length multiplication table")
+// for(let i = 1; i<=len; i++ ){
+//     console.log(table + " x " + i + " = "+ table*i);
+// }
+//========Q-5 ===============
+// let fruits =["apple", "banana", "mango", "orange", "strawberry"]; 
+// for(let i = 0; i <fruits.length; i++){
+//       console.log("Element at index  "+ [i] +" is "+fruits[i]);
+      
+// }
+//============Q-6==========
+//=========== a
+// for(let i = 1; i <=15; i++){
+//     console.log(i);
+    
+// }
+//========= b
+// for(let j = 10; j >=1 ;j--){
+// console.log(j);
+// }
+//======== c
+// for(let i = 0; i <=20; i++){
+//     if(i % 2 === 0){
+//         console.log( i);
+//     }
+// }
+//========== d
+
+// for(let i = 1; i <=20; i++){
+//     if (i % 2 !==0){
+//         console.log(i);
+//     }
+// }
+//============ e
+
+// for(let i = 2 ; i <= 20 ;i++){
+//     if(i % 2 ===0){
+//         document.write(i + "k ,");
+//     }
+    
+// }
+//=======Q-7 ================
+
+// let arr = ["cake", "applepie", "cookie", "chips", "patties"];
+
+// let userInput = prompt("Welcome to our bakerry. what do you want to order sir/ma'am"); 
+
+// let index = arr.indexOf(userInput); 
+
+// if (index !== -1) {
+//   console.log(userInput + " is available at index " + index + ".");
+// } else {
+//   console.log(userInput + " is not in our bakery.");
+// }
+//========= Q-8=====================
+// let arr =  [24, 53, 78, 91, 12]
+// let maxArr = [0];
+// for (let i = 0; i <arr.length; i++ ){
+//     if (arr[i]>maxArr ){
+//         maxArr = arr[i]
+//     }
+// }document.write(`The largest number is ${maxArr} `)
+//==========Q -9=============
+// let arr = [24, 53, 78, 91, 12]
+// let minArr = arr[0];
+// for(let i = 0; i < arr.length ; i++){
+//     if(arr[i]<minArr){
+//         minArr = arr[i]
+//     }
+// }document.write(`The smallest number is ${minArr} `)
+//================Q-10============================
+// for(let i = 1 ; i <=100;i++){
+//     if(i % 5 ===0 ){
+//         console.log(i);
+//     }
+// }
+//==============complete===================
